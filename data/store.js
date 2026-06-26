509d01f417fd71bbd240ba9024ddf0815309cb51
Madhavi1108 <madhavi09307@gmail.com>
2026-06-26 15:29:02 +0530

    feat(store): implement LRU cache strategy for in-memory data

--- data/store.js
+++ data/store.js
@@ -1,12 +1,14 @@
+const LRUCache = require('../server/utils/lruCache');
+
 const store = {
-  culturalItems: [],
-  heritagePaths: [],
-  userProgress: {},
-  villagePosts: [],
-  contributors: [],
-  timelineEvents: [],
-  storySourceData: [],
-  artisans: [],
+  culturalItems: new LRUCache(2000), // Max 2000 items
+  heritagePaths: new LRUCache(1000), // Max 1000 paths
+  userProgress: {}, // Keep as object for fast lookup by userId
+  villagePosts: new LRUCache(1000),
+  contributors: new LRUCache(500),
+  timelineEvents: new LRUCache(500),
+  storySourceData: new LRUCache(500),
+  artisans: new LRUCache(500),
 };
 
 module.exports = store;
